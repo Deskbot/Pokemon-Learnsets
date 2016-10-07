c97a65417dece8bdb1120dd998d9d6af86291403
Thomas Richards <Deskbot@users.noreply.github.com>
2016-10-07 19:05:13 +0100

    makes generate-learnsets more efficient

--- generate-learnset.js
+++ generate-learnset.js
@@ -5,39 +5,38 @@ var targetGen = ps.argv[2]; //index 2 contains the first argument
 
 if (targetGen >= 1) {
 	var moveList = require('./data/learnsets.js');
-	var newMoveList = new Object();
+	var newMoveList = {};
 
 	for (var mon in moveList) {//goes through each Pokémon
-		newMoveList[mon] = new Object();
-		newMoveList[mon].level = new Object();
-		newMoveList[mon].tm = new Array();
-		newMoveList[mon].egg = new Array();
-		newMoveList[mon].tutor = new Array();
-		newMoveList[mon].dreamWorld = new Array();
-		newMoveList[mon].event = new Array();
+		newMoveList[mon] = {
+			level: {},
+			tm: [],
+			egg: [],
+			tutor: [],
+			dreamWorld: [],
+			event: []
+		};
 		
-		for (var move in moveList[mon]) {//goes through each move
-			for (var valKey in moveList[mon][move]) {//goes through each method of obtaining
-				var moveVal = moveList[mon][move][valKey];
+		var move, valKey, moveVal, moveType, moveCount;
+		moveCount = 0;
+		for (move in moveList[mon]) {//goes through each move
+			for (valKey in moveList[mon][move]) {//goes through each method of obtaining
+				moveVal = moveList[mon][move][valKey];
 				
-				if (moveVal.substr(0,1) == targetGen) {
-					//puts move in array based on method of obtaining
-					if (moveVal.substr(1,1) == 'L') {
-						var level = parseInt(moveVal.substr(2,3));
-						newMoveList[mon].level[level] = move;
-					} else if (moveVal.substr(1,1) == 'M') {
-						newMoveList[mon].tm.push(move);
-					} else if (moveVal.substr(1,1) == 'E') {
-						newMoveList[mon].egg.push(move);
-					} else if (moveVal.substr(1,1) == 'T') {
-						newMoveList[mon].tutor.push(move);
-					} else if (moveVal.substr(1,1) == 'D') {
-						newMoveList[mon].dreamWorld.push(move);
-					} else if (moveVal.substr(1,1) == 'S') {
-						newMoveList[mon].event.push(move);
-					} else {
-						console.log('error checking for move types not accounted for:', moveVal.substr(1,1), 'data:', mon, move, valKey, moveVal);
+				if (moveVal.substr(0,1) == targetGen) {//puts move in array based on method of obtaining
+					moveCount++;
+					moveType = moveVal.substr(1,1);
+					
+					if      (moveType === 'L') newMoveList[mon].level[parseInt(moveVal.substr(2,3))] = move;
+					else if (moveType === 'M') newMoveList[mon].tm.push(move);
+					else if (moveType === 'E') newMoveList[mon].egg.push(move);
+					else if (moveType === 'T') newMoveList[mon].tutor.push(move);
+					else if (moveType === 'D') newMoveList[mon].dreamWorld.push(move);
+					else if (moveType === 'S') newMoveList[mon].event.push(move);
+					else {
+						console.error('error checking for move types not accounted for:', moveType, 'data:', mon, move, valKey, moveVal);
 					}
+					//the switch case version looked too ugly
 				}
 			}
 		}
